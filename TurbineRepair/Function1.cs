7ecaba161a7f09da6414fa7f1cea104984c7b715
Glenn Gailey <glenn.gailey@microsoft.com>
2021-05-11 22:57:18 -0700

    remove query params

--- TurbineRepair/Function1.cs
+++ TurbineRepair/Function1.cs
@@ -23,10 +23,6 @@ namespace TurbineRepair
         [FunctionName("TurbineRepair")]
         [OpenApiOperation(operationId: "Run")]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        [OpenApiParameter(name: "hours", In = ParameterLocation.Query, Type = typeof(Int32),
-            Description = "Number of hours since turbine last serviced.")]
-        [OpenApiParameter(name: "capacity", In = ParameterLocation.Query, Type = typeof(Int32),
-            Description = "Kilowatt capacity of turbine.")]
         [OpenApiRequestBody("application/json", typeof(RequestBodyModel), 
             Description = "JSON request body containing { hours, capacity}")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string),
@@ -35,23 +31,18 @@ namespace TurbineRepair
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            // Get query strings if they exist
-            int tempVal;
-            int? hours = Int32.TryParse(req.Query["hours"], out tempVal) ? tempVal : (int?)null;
-            int? capacity = Int32.TryParse(req.Query["capacity"], out tempVal) ? tempVal : (int?)null;
-
             // Get request body
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
 
             // Use request body if a query was not sent
-            capacity = capacity ?? data?.capacity;
-            hours = hours ?? data?.hours;
+            int? capacity = data?.capacity;
+            int? hours = data?.hours;
 
             // Return bad request if capacity or hours are not passed in
             if (capacity == null || hours == null)
             {
-                return new BadRequestObjectResult("Please pass capacity and hours on the query string or in the request body");
+                return new BadRequestObjectResult("Please pass capacity and hours in the request body");
             }
             // Formulas to calculate revenue and cost
             double? revenueOpportunity = capacity * revenuePerkW * 24;
